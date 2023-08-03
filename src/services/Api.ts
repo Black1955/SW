8604901875a67cf4cb7f43b56da3e13e04432c1b
Illia <ichorny.ilya25@gmail.com>
2023-08-03 17:11:22 +0200

    test for api

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import "dotenv";
 export const API = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_URL,
+    baseUrl: "https://socialnetwork.adaptable.app",
     credentials: "include",
   }),
   endpoints: () => ({}),
