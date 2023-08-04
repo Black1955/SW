67b61460a574f8d667acad664e0f4e9aae41980a
Illia <ichorny.ilya25@gmail.com>
2023-08-04 14:59:08 +0200

    change api url

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import "dotenv";
 export const API = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: process.env.REACT_APP_URL,
     credentials: "include",
   }),
   endpoints: () => ({}),
